afbae1c5fea9270e110fe9afca218604360df196
OmarMohamed156 <om658036@gmail.com>
2020-08-01 08:32:46 +0200

    annoy-o-metric

--- Tests/JS/JS_Test.js
+++ Tests/JS/JS_Test.js
@@ -15,3 +15,47 @@ else if(guess < secretNumber){
 else{
  alert("Sorry!!. raise the number");
 }
+ var num1 = -10;
+ while (num1 < 19){
+  console.log(num1);
+  num1++;
+ }
+
+ var num2 = 10;
+ while (num2 < 40 ) {
+  if (num2 % 2 === 0) {
+   console.log(num2);
+  }
+  num2++;
+ }
+
+ var num3 = 300;
+ while (num3 < 333 ) {
+  if (num3 % 2 !== 0) {
+   console.log(num3);
+  }
+  num3++;
+ }
+
+ var num3 = 300;
+ while (num3 < 333 ) {
+  if (num3 % 2 !== 0) {
+   console.log(num3);
+  }
+  num3++;
+ }
+
+ var num4 = 5;
+ while (num4 < 50 ) {
+  if (num4 % 5 === 0) {
+   console.log(num4);
+  }
+  num4++;
+ }
+
+ let annoyMessage = prompt("Are we there yet?");
+ while (annoyMessage.indexOf("yes") === -1 && annoyMessage.indexOf("yeah") === -1) {
+  annoyMessage = prompt("Are we there yet?");
+ }
+
+ alert("yes, we made it..");
